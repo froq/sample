8f313f911ab88cebbc420481db41fd322eda031d
Kerem Güneş <k-gun@mail.com>
2017-07-24 11:44:16 +0300

    Add examples.

--- app/global/cfg.php
+++ app/global/cfg.php
@@ -7,6 +7,11 @@
 return (function() {
     $cfg = [];
 
+    // aliases
+    // $cfg['app.service.aliases']['foo'] = ['REAL_SERVICE_NAME', 'methods' => ['bar' => 'REAL_SERVICE_METHOD_NAME']];
+    // regexp aliases
+    // $cfg['app.service.aliases']['@@@'][] = ['REAL_SERVICE_NAME', 'method' => 'REAL_SERVICE_METHOD_NAME', 'pattern' => '~^/[\w-]+/(\d+)$~i'];
+
     // logger
     $cfg['app.logger']['level'] = Froq\Logger\Logger::FAIL | Froq\Logger\Logger::WARN;
 
