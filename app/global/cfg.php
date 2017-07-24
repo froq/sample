240c407636b8cfa229c21928e4ac3a2f36f28ca9
Kerem Güneş <k-gun@mail.com>
2017-07-24 10:55:14 +0300

    Keep globals clean..

--- app/global/cfg.php
+++ app/global/cfg.php
@@ -2,9 +2,13 @@
 /**
  * Set your config here that will be used globally in app.
  */
-$cfg = [];
 
-// logger
-$cfg['app.logger']['level'] = Froq\Logger\Logger::FAIL | Froq\Logger\Logger::WARN;
+// keep globals clean..
+return (function() {
+    $cfg = [];
 
-return $cfg;
+    // logger
+    $cfg['app.logger']['level'] = Froq\Logger\Logger::FAIL | Froq\Logger\Logger::WARN;
+
+    return $cfg;
+})();
