37ea7e98c5ce8fc754618f68d8230e2c1e555900
Kerem Güneş <k-gun@mail.com>
2017-07-28 19:47:45 +0300

    Comment out all.

--- app/service/BookService/model/model.php
+++ app/service/BookService/model/model.php
@@ -6,30 +6,30 @@ use Froq\Database\Model\Oppa as Model;
 /**
  * Book Model.
  */
-class BookModel extends Model
-{
-    // vendor name
-    protected $vendorName = 'mysql';
+// class BookModel extends Model
+// {
+//     // vendor name
+//     protected $vendorName = 'mysql';
 
-    // table name & table primary for mysql
-    protected $stack = 'book';
-    protected $stackPrimary = 'id';
+//     // table name & table primary for mysql
+//     protected $stack = 'book';
+//     protected $stackPrimary = 'id';
 
-    /**
-     * Init
-     * @return void
-     */
-    public function init()
-    {
-        // manipulate pager options if needed
-        // $app = $this->service->getApp();
-        // $this->pager
-        //     ->setAutorun(false)
-        //     ->setNumerateFirstLast(true)
-        //     ->setStart((int) $app->getRequest()->getParam('start'))
-        //     ->setStop((int) $app->getRequest()->getParam('stop'))
-        //     // or
-        //     ->setStop(5)
-        // ;
-    }
-}
+//     /**
+//      * Init
+//      * @return void
+//      */
+//     public function init()
+//     {
+//         // manipulate pager options if needed
+//         // $app = $this->service->getApp();
+//         // $this->pager
+//         //     ->setAutorun(false)
+//         //     ->setNumerateFirstLast(true)
+//         //     ->setStart((int) $app->getRequest()->getParam('start'))
+//         //     ->setStop((int) $app->getRequest()->getParam('stop'))
+//         //     // or
+//         //     ->setStop(5)
+//         // ;
+//     }
+// }
