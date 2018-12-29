4794115a0b9c358cb9ff796b64e98b1b4aef8e50
Kerem Güneş <k-gun@mail.com>
2018-12-29 04:21:17 +0300

    Remove 'app.' prefix from $cfg keys.

--- app/global/cfg.php
+++ app/global/cfg.php
@@ -8,16 +8,16 @@ return (function() {
     $cfg = [];
 
     // aliases
-    // $cfg['app.service.aliases']['foo'] = ['<REAL_SERVICE_NAME>', 'methods' => ['bar' => '<REAL_SERVICE_METHOD_NAME>']];
+    // $cfg['service.aliases']['foo'] = ['<REAL_SERVICE_NAME>', 'methods' => ['bar' => '<REAL_SERVICE_METHOD_NAME>']];
     // regexp aliases
-    // $cfg['app.service.aliases']['@@@'][] = ['<REAL_SERVICE_NAME>', 'method' => '<REAL_SERVICE_METHOD_NAME>', 'pattern' => '~^/[\w-]+/(\d+)$~i'];
+    // $cfg['service.aliases']['@@@'][] = ['<REAL_SERVICE_NAME>', 'method' => '<REAL_SERVICE_METHOD_NAME>', 'pattern' => '~^/[\w-]+/(\d+)$~i'];
 
     // session save handler
-    // $cfg['app.session']['saveHandler'] = '\My\Session\SaveHandler'; // or
-    // $cfg['app.session']['saveHandler'] = ['\My\Session\SaveHandler', '/my/session/save-handler.php'];
+    // $cfg['session']['saveHandler'] = '\My\Session\SaveHandler'; // or
+    // $cfg['session']['saveHandler'] = ['\My\Session\SaveHandler', '/my/session/save-handler.php'];
 
     // logger
-    $cfg['app.logger']['level'] = Froq\Logger\Logger::FAIL | Froq\Logger\Logger::WARN;
+    $cfg['logger']['level'] = Froq\Logger\Logger::FAIL | Froq\Logger\Logger::WARN;
 
     return $cfg;
 })();
