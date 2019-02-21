efc9474478b680454e1109599878638402a929bd
Kerem Güneş <k-gun@mail.com>
2019-02-22 01:57:42 +0300

    Update cfg.

--- app/global/cfg.php
+++ app/global/cfg.php
@@ -8,16 +8,16 @@ return (function() {
     $cfg = [];
 
     // aliases
-    // $cfg['service.aliases']['foo'] = ['<REAL_SERVICE_NAME>', 'methods' => ['bar' => '<REAL_SERVICE_METHOD_NAME>']];
-    // regexp aliases
-    // $cfg['service.aliases']['~~'][] = ['<REAL_SERVICE_NAME>', 'method' => '<REAL_SERVICE_METHOD_NAME>', 'pattern' => '~^/[\w-]+/(\d+)$~i'];
+    // $cfg['service']['aliases']['foo'] = ['<REAL_SERVICE_NAME>', 'methods' => ['bar' => '<REAL_SERVICE_METHOD_NAME>']];
+    // aliases with regexp
+    // $cfg['service']['aliases']['~~'][] = ['<REAL_SERVICE_NAME>', 'method' => '<REAL_SERVICE_METHOD_NAME>', 'pattern' => '~^/[\w-]+/(\d+)$~i'];
 
     // session save handler
     // $cfg['session']['saveHandler'] = '\My\Session\SaveHandler'; // or
     // $cfg['session']['saveHandler'] = ['\My\Session\SaveHandler', '/my/session/save-handler.php'];
 
     // logger
-    $cfg['logger']['level'] = froq\logger\Logger::FAIL | froq\logger\Logger::WARN;
+    // $cfg['logger']['level'] = froq\logger\Logger::ALL;
 
     return $cfg;
 })();
