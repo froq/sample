33b603029d94716752a2a12ffbbcc5f1f1035fc4
Kerem Güneş <qeremy@gmail.com>
2020-06-10 23:23:12 +0300

    bin/server: fix __local__ position.

--- bin/server.php
+++ bin/server.php
@@ -9,6 +9,9 @@ if (PHP_SAPI != 'cli-server') {
     die('This file must be run via cli-server only.');
 }
 
+// Set env as local.
+define('__local__', true);
+
 $_pub = realpath(__dir__ . '/../pub');
 
 // Check for static files.
@@ -16,9 +19,6 @@ $_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (file_exists($_pub . $_uri)) {
     return false;
 }
-
-// Set env as local.
-define('__local__', true);
 
 // Forward all to index.php.
 require $_pub . '/index.php';
