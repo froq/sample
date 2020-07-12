adad313b98fd3f175828f11b3d1e14f3c1de2e30
Kerem Güneş <qeremy@gmail.com>
2020-07-12 05:07:52 +0300

    Optimize bin/server.php.

--- bin/server.php
+++ bin/server.php
@@ -6,19 +6,19 @@
 // ~$ sudo -u www-data php -S localhost:8080 -t pub/ bin/server.php
 
 if (PHP_SAPI != 'cli-server') {
-    die('This file must be run via cli-server only.');
+    die('This file must be run via cli-server only!');
+}
+
+$_pub = realpath(__dir__ . '/../pub');
+$_uri = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+
+// Check for static files.
+if (file_exists($_pub . $_uri)) {
+    return false;
 }
 
 // Set env as local.
 define('__local__', true);
-
-$_pub = realpath(__dir__ . '/../pub');
-
-// Check for static files.
-$_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if (file_exists($_pub . $_uri)) {
-    return false;
-}
 
 // Forward all to index.php.
 require $_pub . '/index.php';
