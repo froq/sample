f299d68375a20e280af110b8eb0c94a0725f4d2a
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-19 16:33:32 +0300

    Add app/Froq.php & update index.php files.

--- pub/index.php
+++ pub/index.php
@@ -1,72 +1,37 @@
-<?php
-// Define app dir & start time.
-define('APP_DIR', dirname(__dir__));
-define('APP_START_TIME', microtime(true));
+<?php declare(strict_types=1);
 
-// Include composer autoload.
-if (!is_file(APP_DIR . '/vendor/autoload.php')) {
-    die('Composer autoload file not found!');
-}
-require APP_DIR . '/vendor/autoload.php';
+/** Define app dir & start. */
+define('APP_DIR', dirname(__DIR__));
+define('APP_START', microtime(true));
 
-// Include autoloader file.
-if (!is_file($file = (APP_DIR . '/vendor/froq/froq/src/Autoloader.php'))) {
-    die('Froq autoloader file "' . $file . '" not found!');
-}
-require $file;
+/** Require Froq! */
+require APP_DIR . '/app/Froq.php';
 
-// Register autoloader.
-$autoloader = froq\Autoloader::init();
-$autoloader->register();
-
-// Include initial file that returns App object.
-if (!is_file($file = (APP_DIR . '/vendor/froq/froq/src/_init.php'))) {
-    die('Froq init file "' . $file . '" not found!');
-}
-
-/**
- * App object.
- * @var froq\App
- */
-$app = require $file;
-
-/**
- * App root (for API versioning or such works eg: /v1 => api.foo.com/v1).
- * @var string
- */
-$app_root = '/';
-
-/**
- * App environment.
- * @var string
- */
-$app_env = __local__ ? froq\Env::DEVELOPMENT : froq\Env::PRODUCTION;
-
-/**
- * App configurations (those environmental when environment-related file exists).
- * @var array
- */
-$app_configs = is_file(APP_DIR . '/app/config/config-' . $app_env . '.php')
-    ? require APP_DIR . '/app/config/config-' . $app_env . '.php'
-    : require APP_DIR . '/app/config/config.php';
-
-// Error handler.
-// $app->on('error', function ($event, $error) { ... });
-
-// Output handler.
-// $app->on('output', function ($event, $output) { ... });
-
-// Before/after handlers.
-// $app->on('before', function ($event) { ... });
-// $app->on('after', function ($event) { ... });
-
-// Shortcut routes.
-// $app->get('/book/:id', 'Book.show');
-// $app->get('/book/:id', function ($id) { ... });
-
-// Run app.
 try {
-    $app->run($app_root, $app_env, $app_configs);
+    /** Initialize. */
+    Froq::init(root: '/', env: null, dir: null)
+
+    /** Apply preparations. */
+    // ->apply(function ($app) {
+    //     // Error handler.
+    //     $app->on('error', function ($event, $error) { ... });
+
+    //     // Output handler.
+    //     $app->on('output', function ($event, $output) { ... return $output });
+
+    //     // Before/after handlers.
+    //     $app->on('before', function ($event) { ... });
+    //     $app->on('after', function ($event) { ... });
+
+    //     // Shortcut routes.
+    //     $app->get('/book/:id', 'Book.show');
+    //     $app->get('/book/:id', function ($id) { ... });
+    // })
+
+    /** Run. */
+    ->run();
 } catch (Throwable $e) {
-    $app->rerun($e);
+    error_log((string) $e);
+    // echo $e->getMessage(), "\n";
+    // echo $e->getTraceAsString();
 }
