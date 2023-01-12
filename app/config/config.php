3bf24a000989af36a7799e41c661e6a93e0b6fd7
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2023-01-13 01:22:07 +0300

    Make-ups.

--- app/config/config.php
+++ app/config/config.php
@@ -3,29 +3,29 @@
  * Define your configs here.
  */
 return [
-    // Note: dot (.) notations are valid for base keys.
-    // With/without dots.
-    // 'response' => ['json' => ['flags' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE]]
-    // 'response.json' => ['flags' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE]
+    /* Note: dot (.) notations are valid for base keys. */
+    /* With/without dots. */
+    /* 'response' => ['json' => ['flags' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE]] */
+    /* 'response.json' => ['flags' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE] */
 
-    // Routes & services.
+    /* Routes & services. */
     'routes'   => include 'routes.php',
     'services' => include 'services.php',
 
-    // Protocols.
+    /* Protocols. */
     'http'     => 'http://' . $_SERVER['SERVER_NAME'],
     'https'    => 'https://' . $_SERVER['SERVER_NAME'],
 
-    // Localization.
+    /* Localization. */
     'timezone' => 'UTC',
     'language' => 'en',
     'encoding' => 'UTF-8',
     'locales'  => [LC_TIME => 'en_US.UTF-8'],
 
-    // Ini.
+    /* Ini. */
     // 'ini' => [string => scalar],
 
-    // Initial response headers (null means remove).
+    /* Initial response headers (null means remove). */
     'headers'  => [
         // Cache (https://stackoverflow.com/questions/49547/how-do-we-control-web-page-caching-across-all-browsers).
         'Cache-Control'          => 'no-cache, no-store, must-revalidate, max-age=0, pre-check=0, post-check=0',
@@ -39,55 +39,54 @@ return [
         'X-XSS-Protection'       => '1; mode=block',
     ],
 
-    // Initial response cookies.
+    /* Initial response cookies. */
     // 'cookies'  => [name => [value, ?[options]]],
 
-    // Log options.
+    /* Log options. */
     // 'log' => [
     //     'level'     => froq\log\LogLevel::ERROR | froq\log\LogLevel::WARN,
-    //     'directory' => APP_DIR .'/var/log',
+    //     'directory' => APP_DIR . '/var/log',
     // ],
 
-    // Session options ([] = use default options).
-    // 'session'  => [],
-    // With custom options (below is default).
+    /* Session options (true = activate with default options). */
+    // 'session'  => true,
+    /* With custom options (below is default). */
     // 'session'  => [
-    //     'name'        => 'SID',
-    //     'hash'        => bool or 'uuid', 'hashUpper' => bool,
-    //     'hashLength'  => 40, // ID length: 16, 32 or 40 only.
-    //     'savePath'    => null, 'saveHandler' => class or [class, class file],
-    //     'cookie'      => [
+    //     'name'     => 'SID',
+    //     'hash'     => 'uuid' or 32, 40, 16, 'hashUpper' => bool,
+    //     'savePath' => null, 'saveHandler' => class or [class, class file],
+    //     'cookie'   => [
     //         'lifetime' => 0,     'path'     => '/',   'domain'   => '',
     //         'secure'   => bool,  'httponly' => bool,  'samesite' => '',
     //     ],
     // ],
 
-    // Response options.
+    /* Response options. */
     // 'response.compress'       => ['gzip' or 'zlib', 'minlen' => 64 (in kb) 'level' => 1..9],
     // 'response.xml'            => ['indent' => bool, 'indentString' => ' '],
     // 'response.json'           => ['indent' => string|int, 'flags' => JSON_*],
     // 'response.file.rateLimit' => 1024 ** 2, // 1MB
 
-    // Database options.
+    /* Database options. */
     // 'database'    => [
-    //     'dsn'  => '...',
-    //     'user' => '', 'pass' => '', 'profile' => bool,
+    //     'dsn'  => string,
+    //     'user' => string, 'pass' => string, 'profile' => bool,
     // ],
 
-    // Cache options.
-    // 'cache' => ['id' => string, 'options' => ['id' => string?, 'agent' => string, 'static' => bool,
+    /* Cache options. */
+    // 'cache' => ['id' => string, 'options' => ['id' => ?string, 'agent' => string, 'static' => bool,
     //     'ttl' => int, ...]],
 
-    // Router options.
+    /* Router options. */
     // 'router' => ['defaultController' => string, 'defaultAction' => string,
     //              'unicode' => bool, 'decodeUri' => bool, 'endingSlashes' => bool],
 
-    // Dot-env.
+    /* Dot-env. */
     // 'dotenv' => ['file' => string, 'global' => bool],
 
-    // View options.
+    /* View options. */
     // 'view' => ['base' => string, 'layout' => string],
 
-    // Misc options.
+    /* Misc options. */
     'exposeAppRuntime' => true, // true=all, false=none or 'development', 'testing', 'staging', 'production'
 ];
