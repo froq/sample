242e5d616e16ba3653a459876e670bc1ec2275cb
Kerem Güneş <1508306+krmgns@users.noreply.github.com>
2024-08-17 20:31:17 +0300

    Apply service & injection optimisations.

--- app/service/HelloInterface.php
+++ app/service/HelloInterface.php
@@ -3,7 +3,7 @@
 namespace app\service;
 
 /**
- * Sample service interface to demonstrate service injections
+ * Sample service interface to demonstrate service injections.
  */
 interface HelloInterface {
     function say(string $name = null): string;
